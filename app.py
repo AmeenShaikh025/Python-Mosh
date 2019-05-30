3d096255cef8920cc79d1e033d5d598fe5023576
Ameen Shaikh <34180338+AmeenShaikh025@users.noreply.github.com>
2019-05-30 09:46:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,84 @@
+# print('Ameen')
+# print('0----')
+# print(' ||||')
+# print('*' * 10)
+
+#Variables
+
+# price = 10
+# print(price)
+# price = 20
+# print(price)
+# rating = 4.9
+# name = 'Ameen'
+# is_published = False
+# is_publish = True
+#
+# pat_name = 'John Smith'
+# age = 20
+# is_new_patient = True
+
+#Input
+
+# name = input('What is your name? ')
+# print('Hi ' + name)
+#
+# person_name = input('What is your name? ')
+# fav_color = input('what is your favorite color ? ');
+# print(person_name +' likes '+ fav_color)
+
+#Error(String)
+#int()
+#float()
+#bool()
+
+#conversion
+
+# birth_year = input('Birth Year: ')
+# print(type(birth_year))
+# age = 2019 - int(birth_year)
+# print(type(age))
+# print(age)
+
+# weight_pounds = input('Enter your weight in pounds: ')
+# weight_kilogram = float(weight_pounds) * .45
+# print(weight_kilogram)
+
+
+#strings
+
+# course = "Python's course for Biginners"
+# print(course)
+
+# email = '''
+#     Hi John,
+#
+#     Here is our first email to you
+#
+#     Thank you
+#     The support team
+# '''
+# print(email)
+
+course = "Python for Biginners"
+print(course[0]) #p
+print(course[-1]) #s
+print(course[-2]) #r
+
+print(course[0:3]) #Pyt
+
+print(course[0:]) #Python for Biginners ( DEFAULT END INDEX IS -1)
+print(course[1:]) #ython for Biginners
+
+print(course[:5]) #Pytho   ( DEFAULT START INDEX IS 0 )
+print(course[:]) #Python for Biginners
+
+another = course[:]
+print(another)
+
+name = 'Jennifer'
+
+print(name[1:-1]) #ennife
+
+#Formatted strings
+
